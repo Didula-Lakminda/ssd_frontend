dd99dee1ec4c271ab7e164c6f4c7c1067a314386
DidulaLakminda <didula@niogin.com>
2022-11-07 10:11:41 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import './assets/libs/boxicons-2.1.1/css/boxicons.min.css'
+import './scss/App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import Blank from './pages/Blank';
+
 function App() {
   return (
-    <div>
-      <h1>My App</h1>
-    </div>
+    <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route path="orders" element={<Blank />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
   );
 }
 
